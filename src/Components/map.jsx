1baf2ccdd8e0f192c59cd04806f4acf1a4de6ad5
dum <129909927+Smoothn@users.noreply.github.com>
2024-09-18 17:00:22 -0300

    Agregando latitud y longitud
    long y lat de epet20 agregada

--- src/Components/map.jsx
+++ src/Components/map.jsx
@@ -8,12 +8,14 @@ import {
 
     const Map = (props) => {
         return (
-            <GoogleMap/>
+            <GoogleMap defaultZoom={10}
+            defaultCenter={{ lat :-38.9648026 , lng :-68.0890925 }}
+            />
         );
     };
 
     export default withScripJs(
         withGoogleMaps(
-            Map 
+            Map
         )
     )
