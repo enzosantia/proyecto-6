c24a4faa97f2c9d85109c71198fefca29f0a2445
dum <129909927+Smoothn@users.noreply.github.com>
2024-09-18 16:52:44 -0300

    Agregando Export

--- src/Components/map.jsx
+++ src/Components/map.jsx
@@ -5,3 +5,15 @@ import {
         withGoogleMaps
 
 }   from 'reac-google-maps'; 
+
+    const Map = (props) => {
+        return (
+            <GoogleMap/>
+        );
+    };
+
+    export default withScripJs(
+        withGoogleMaps(
+            Map 
+        )
+    )
