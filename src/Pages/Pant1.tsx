50cf8da3f0fe9c54354ef6ada7c227d184b62d10
dum <129909927+Smoothn@users.noreply.github.com>
2024-09-19 16:56:51 -0300

    Mejorando codigo
    Rehaciendo el mapa en react native

--- src/Pages/Pant1.tsx
+++ src/Pages/Pant1.tsx
@@ -3,6 +3,7 @@ import { View, StyleSheet, Image } from "react-native";
 
 import Map from "../../Map";
 import Credentials from '../../Credenciales';
+// import MapView, {Marker, Polyline} from 'react-native-maps';
 
 import { Encab } from "../Components/Encabezado";
 import { Boton } from "../Components/BotonPiePan1";
@@ -16,31 +17,42 @@ export default function Pan1() {
     </View>
   );
 };
-/*
-const MapURL = `https://maps.googleapis.com/maps/api/js?v=3.exp&key=${Credentials.mapsKey}`;
 
-class App extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      name: 'React'
-    };
-  }
+/* 
+export default function Pan1() {
 
-  render() {
-    return (
-      <div>
-        <Map
-          googleMapURL={MapURL}
-          containerElement={<div style={{ height: '400px' }} />}
-          mapElement={<div style={{ height: '100%' }} />}
-          loadingElement={<p>Cargando</p>}
-        />
-      </div>
-    );
-  }
-}
+// Iniciar mapa en las cords de la EPET20
+
+  const [origin, setOrigin] = React.useState({
+  latitute: -38.9648026,
+  longitude: -68.0890925,
+  });
+  return (
+    <View style={styles.container}>
+      <MapView 
+      style={styles.map} 
+      initialRegion={{
+      latitude: origin.latitude,
+      longitude: origin.longitude,
+      latitudeDelta: 0.09,
+      longitudeDelta: 0.04
+      }}
+      >
+
+      // Coords de comisarias marcadas en el mapa
+
+      <Marker
+        coordinate={"COMISARIA LAT Y LONG"}
+      />
+            <Marker
+        coordinate={""}
+      />
+
+      </MapView>
+      
+    </View>
 */
+
 
 const styles = StyleSheet.create({
   container: {
